9538754fe35045b54ad3edf37efa202a4d0adbe8
Narasimha <pnara527@gmail.com>
2019-11-04 08:03:59 +0530

    Asp.Net-MVC-core-with-Azure-Cosmos-databae-CRUD-
    Asp.Net-MVC-core-with-Azure-Cosmos-databae-CRUD-

---
+++ MVCCosmosSQLCoreDB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCCosmosSQLCoreDB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVCCosmosSQLCoreDB/Services/ICosmosDbService.cs
@@ -0,0 +1,18 @@
+﻿using MVCCosmosSQLCoreDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCosmosSQLCoreDB.Services
+{
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
